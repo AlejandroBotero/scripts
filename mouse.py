d4e6f3abcb9858082e94094bf9ec81ea9d77e77d
working_i <haljob35@gmail.com>
2024-02-02 09:43:47 -0500

    some changes made

--- mouse.py
+++ mouse.py
@@ -2,7 +2,7 @@ import keyboard
 import pyautogui as pg
 
 
-px = 100
+conf = {'px':100}
 
 def move_x(pxtomv):
     pg.move(pxtomv, 0)
@@ -13,14 +13,30 @@ def move_y(pxtomv):
 def on_event(event):
 
     n= event.name
+
+    #movement
     if n == 'h':
-        move_x(-px)
+        move_x(-conf['px'])
     elif n == 'l':
-        move_x(px)
+        move_x(conf['px'])
     elif n == 'j':
-        move_y(px)
+        move_y(conf['px'])
     elif n == 'k':
-        move_y(-px)
+        move_y(-conf['px'])
+    
+    #gear
+    elif n == 'a':
+        conf['px'] = 5
+    elif n == 'o':
+        conf['px'] = 35
+    elif n == 'e':
+        conf['px'] = 75
+    elif n == 'u':
+        conf['px'] = 125
+
+    #events
+    elif n == 't':
+        pg.click()
 
 keyboard.on_press(on_event)
-keyboard.wait()
+keyboard.wait('esc')
